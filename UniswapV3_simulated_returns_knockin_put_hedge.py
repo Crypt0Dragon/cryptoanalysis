68b9b679dee134b8e58a29193754e0f7c9329c22
Crypt0Dragon <eu.chinghwa@gmail.com>
2022-08-07 11:36:25 +0100

    added Monte Carlo simulation for Uniswap V3 LP hedging strategy, Black-Scholes KnockIn Put Pricer

---
+++ UniswapV3_simulated_returns_knockin_put_hedge.py
@@ -0,0 +1,304 @@
+import streamlit as st
+st.set_page_config(page_title="UniSwapV3 Simulated Returns", layout="wide")
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+import pandas as pd
+import scipy.stats as si
+
+st.title("UniSwapV3 Simulated Returns, LP hedging with knockin put option")
+
+P = st.number_input('Current Price', value=1666.000, step = 0.001)
+y = st.number_input('Number of USD tokens', value=1000000, step = 1)
+
+col1, col2 = st.columns(2)
+P_min = col1.number_input("Min Price",value=1333.000, step = 0.001)
+P_max = col2.number_input("Max Price",value=1650.000, step = 0.001)
+
+L=(np.sqrt(P_min)/min(P,P_max) + 1/np.sqrt(min(P,P_max)))/(1-P_min/min(P,P_max))*y                                                                                 
+x_min = L*(1/np.sqrt(P_min)-1/np.sqrt(P_max))
+P_DCA = y/x_min
+col1, col2 = st.columns(2)
+col1.write("Strike (DCA price when price of token hits min price)")
+col2.text(P_DCA)
+col1, col2 = st.columns(2)
+col1.write("Number of puts:")
+col2.text(x_min)
+
+st.subheader("Enter your hedge: ")
+
+col1, col2, col3 = st.columns(3)
+#Set your own number of puts
+x_min_new = col1.number_input('Number of puts', value=x_min, step = 0.001)
+
+#Set your own put strike 
+P_put_new = col2.number_input('Put strike', value=P_DCA, step = 0.001)
+
+#Set your knock in barrier (set to lower bound P_min to exactly hedge the left tail)
+B_knockin = col3.number_input('Down and In barrier', value=P_min, step = 0.001)
+
+st.subheader("Enter your Monte-Carlo simulation parameters: ")
+
+r_in_percent = st.number_input('Drift in % (risk-free rate for option pricing)', value=0.0000, step = 0.0001)
+r = r_in_percent/100
+sigma_in_percent = st.number_input('Volatility in %', value = 90.000, step=0.0001)
+sigma = sigma_in_percent / 100
+numberdays = st.number_input("Number of days", value = 7, step = 1)
+steps_per_day = st.number_input("Number of time steps per days for simulation", value = 20, step = 1)
+number_paths = st.number_input("Number of simulated paths", value = 10000, step = 1)
+APR_in_percent  = st.number_input("Enter APR in %", value = 250.00, step = 0.001)
+APR = APR_in_percent/100
+
+
+
+lower_bound = P_min/10000
+upper_bound = 2.1*P_max
+step = upper_bound/10000
+prices = np.arange(P_min/1000,2*P_max+1,step)
+
+pnl = []
+for price in prices:
+    x_future = max(0,L*(min(1/np.sqrt(P_min),1/np.sqrt(price))-1/np.sqrt(P_max)))
+    y_future = max(0,L*(min(np.sqrt(P_max),np.sqrt(price))-np.sqrt(P_min)))
+    pnl.append(y_future + x_future * price - y)
+
+df = pd.DataFrame()
+df["Price"] = prices
+df["PNL"] = pnl
+
+col1, col2 =st.columns(2)
+fig = px.line(df, x="Price", y="PNL", title='Impermanent loss without hedging')
+fig.update_xaxes(title_text='Price')
+fig.update_yaxes(title_text='PNL')
+col1.plotly_chart(fig,use_container_width=True)
+
+pnl_hedged_put_vanilla = []
+pnl_hedged_put_knockin =[]
+for price in prices:
+    x_future = max(0,L*(min(1/np.sqrt(P_min),1/np.sqrt(price))-1/np.sqrt(P_max)))
+    y_future = max(0,L*(min(np.sqrt(P_max),np.sqrt(price))-np.sqrt(P_min)))
+    pnl_hedged_put_vanilla.append(y_future + x_future * price  - y + x_min_new * max(0,P_put_new-price))
+    pnl_hedged_put_knockin.append(y_future + x_future * price  - y + x_min_new * (P_put_new-price if price <= B_knockin else 0))
+
+df = pd.DataFrame()
+df["Price"] = prices
+df["PNL Vanilla Put Hedge"] = pnl_hedged_put_vanilla
+df["PNL Knockin Put Hedge"] = pnl_hedged_put_knockin
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df["Price"], y=df["PNL Vanilla Put Hedge"], mode='lines', name="Vanilla Put Hedge"))
+fig.add_trace(go.Scatter(x=df["Price"], y=df["PNL Knockin Put Hedge"], mode='lines', name="Knockin Put Hedge"))
+
+fig.update_layout(title='Impermanent loss, LP with put hedging<br>Number of puts: ' + str(x_min_new) + ', Strike = ' + str(P_put_new) + ', Down and in barrier = ' + str(B_knockin) + '<br>Notional protection: ' + str(x_min_new * P_put_new) )
+fig.update_xaxes(title_text='Price')
+fig.update_yaxes(title_text='PNL')
+col2.plotly_chart(fig,use_container_width=True)
+
+
+with st.form("generate_mc_simulaton"):
+
+    submitted = st.form_submit_button("Generate Monte Carlo Simulation")
+    if submitted:
+        st.subheader("Simulated results")
+        def gen_paths(S0, r, sigma, T, M, I):
+            dt = float(T) / M
+            paths = np.zeros((M + 1, I), np.float64)
+            paths[0] = S0
+            for t in range(1, M + 1):
+                rand = np.random.standard_normal(I)
+                paths[t] = paths[t - 1] * np.exp((r - 0.5 * sigma ** 2) * dt +
+                                                sigma * np.sqrt(dt) * rand)
+            return paths
+
+        def portfolio_value(price):
+            x_future = max(0,L*(min(1/np.sqrt(P_min),1/np.sqrt(price))-1/np.sqrt(P_max)))
+            y_future = max(0,L*(min(np.sqrt(P_max),np.sqrt(price))-np.sqrt(P_min)))
+            return y_future + x_future * price  
+
+        #Starting price
+        S0 = P
+
+        # time to maturity expressed in years
+        T = numberdays/365.25
+
+        # total number of time steps
+        M = numberdays * steps_per_day
+
+        # time interval for each time step, expressed in years
+        dt = T / M
+
+        mc_paths = pd.DataFrame(gen_paths(S0,r,sigma,T,M,number_paths).transpose())
+        mc_portfolio_values = pd.DataFrame()
+        for i in range(0, M + 1):
+            mc_portfolio_values_time = []
+            prices = mc_paths[i]
+            for j in range(0, number_paths):
+                mc_portfolio_values_time.append(portfolio_value(prices[j]))
+            mc_portfolio_values = pd.concat([mc_portfolio_values, pd.Series(mc_portfolio_values_time)], axis = 1)
+        mc_portfolio_values.columns = range(0 , M + 1)    
+
+        earnings = pd.DataFrame()
+        for i in range(0, M + 1):
+            earnings_time = []
+            portfolio_values = mc_portfolio_values[i]
+            for j in range(0, number_paths):
+                if mc_paths[i][j] < P_min or mc_paths[i][j] > P_max:
+                    earnings_time.append(0)
+                else:
+                    earnings_time.append(APR*dt*portfolio_values[j])
+            earnings = pd.concat([earnings, pd.Series(earnings_time)], axis = 1)
+        earnings.columns = range(0, M+1)    
+
+        #rows: Monte carlo paths
+        #columns: generated fee at time step n = value of LP position* APR * dt if price in range
+        earnings["Total earnings"] = earnings.sum(axis=1) 
+        earnings["Number of days in range"] = earnings.gt(0).sum(axis=1) * 1/steps_per_day
+
+        col1, col2, col3 = st.columns([2,1,1])        
+        df = px.data.tips()
+        fig = px.histogram(earnings["Total earnings"], title = "Fees generated<br>Number of simulations: " + str(number_paths))
+        col1.plotly_chart(fig,use_container_width=True)
+
+        col2.text("Average fees generated: ")
+        col3.text(earnings["Total earnings"].mean())
+        col2.text("Median fees generated: ")
+        col3.text(earnings["Total earnings"].median())
+        col2.text("5th percentile fees generated: ")
+        col3.text(earnings["Total earnings"].quantile(.05))
+        col2.text("1st percentile fees generated: ")
+        col3.text(earnings["Total earnings"].quantile(.01))
+
+        col1, col2, col3 = st.columns([2,1,1])      
+        df = px.data.tips()
+        fig = px.histogram(earnings["Number of days in range"], title = "Number of days in range<br>Number of simulations: " + str(number_paths))
+        col1.plotly_chart(fig,use_container_width=True)
+
+
+        col2.text("Average number of days in range: ")
+        col3.text(earnings["Number of days in range"].mean())
+
+        col2.text("Median number of days in range: ")
+        col3.text(earnings["Number of days in range"].median())
+
+        col2.text("5th percentile number of days in range: ")
+        col3.text(earnings["Number of days in range"].quantile(.05))
+
+        col2.text("1st percentile number of days in range: ")
+        col3.text(earnings["Number of days in range"].quantile(.01))
+
+        mc_paths["earnings"] = earnings["Total earnings"]
+        pnl = []
+        for price in mc_paths[M]:
+            x_future = max(0,L*(min(1/np.sqrt(P_min),1/np.sqrt(price))-1/np.sqrt(P_max)))
+            y_future = max(0,L*(min(np.sqrt(P_max),np.sqrt(price))-np.sqrt(P_min)))
+            pnl.append(y_future + x_future * price - y)
+        mc_paths["PnL"] = pnl + mc_paths["earnings"]    
+
+        col1, col2, col3 = st.columns([2,1,1])
+        df = px.data.tips()
+        fig = px.histogram(mc_paths["PnL"], title = "PnL including generated fees, unhedged<br>Number of simulations: " + str(number_paths))
+        col1.plotly_chart(fig,use_container_width=True)
+        
+        col2.text("Average PnL, unhedged LP: ")
+        col3.text(mc_paths["PnL"].mean())
+
+        col2.text("Median PnL, unhedged LP: ")
+        col3.text(mc_paths["PnL"].median())
+
+        col2.text("5th percentile PnL, unhedged LP: ")
+        col3.text(mc_paths["PnL"].quantile(.05))
+
+        col2.text("1st percentile PnL, unhedged LP: ")
+        col3.text(mc_paths["PnL"].quantile(.01))
+
+        pnl_hedged_put_vanilla  = []
+        for price in mc_paths[M]:
+            x_future = max(0,L*(min(1/np.sqrt(P_min),1/np.sqrt(price))-1/np.sqrt(P_max)))
+            y_future = max(0,L*(min(np.sqrt(P_max),np.sqrt(price))-np.sqrt(P_min)))
+            pnl_hedged_put_vanilla.append(y_future + x_future * price  - y + x_min_new * max(0,P_put_new-price))
+
+        #y_future + x_future * price  - y = impermanent loss
+        #x_min_new * max(0,P_put_new-price) = put payoff
+        #mc_paths["earnings"]  = generated fees
+        mc_paths["PnL Vanilla Put Hedge"] = pnl_hedged_put_vanilla + mc_paths["earnings"]    
+
+        col1, col2, col3 = st.columns([2,1,1])
+        df = px.data.tips()
+        fig = px.histogram(mc_paths["PnL Vanilla Put Hedge"], title = "PnL including generated fees, hedged with " + str(x_min_new) + " vanilla puts, <br>Strike: " +str(P_put_new) + "<br>Number of simulations: " + str(number_paths))
+        col1.plotly_chart(fig,use_container_width=True)
+
+        col2.text("Average PnL, hedged LP with vanilla put: ") 
+        col3.text(mc_paths["PnL Vanilla Put Hedge"].mean())
+        col2.text("Median PnL, hedged LP with vanilla put: ") 
+        col3.text(mc_paths["PnL Vanilla Put Hedge"].median())
+        col2.text("5th percentile PnL, hedged LP with vanilla put: ") 
+        col3.text(mc_paths["PnL Vanilla Put Hedge"].quantile(.05))
+        col2.text("1st percentile PnL, hedged LP with vanilla put: ") 
+        col3.text(mc_paths["PnL Vanilla Put Hedge"].quantile(.01))        
+
+        pnl_hedged_put_knockin  = []
+        for price in mc_paths[M]:
+            x_future = max(0,L*(min(1/np.sqrt(P_min),1/np.sqrt(price))-1/np.sqrt(P_max)))
+            y_future = max(0,L*(min(np.sqrt(P_max),np.sqrt(price))-np.sqrt(P_min)))
+            pnl_hedged_put_knockin.append(y_future + x_future * price  - y + x_min_new * (P_put_new-price if price <= B_knockin else 0))
+
+        #y_future + x_future * price  - y = impermanent loss
+        #x_min_new * (P_put_new-price if price <= B_knockin else 0) = knockin put payoff
+        #mc_paths["earnings"]  = generated fees
+        mc_paths["PnL Knockin Put Hedge"] = pnl_hedged_put_knockin + mc_paths["earnings"]            
+
+        col1, col2, col3 = st.columns([2,1,1])
+        df = px.data.tips()
+        fig = px.histogram(mc_paths["PnL Knockin Put Hedge"], title = "PnL including generated fees, hedged with " + str(x_min_new) + " knockin puts, <br>Strike: " +str(P_put_new) + ', Down and in barrier = ' + str(B_knockin) + "<br>Number of simulations: " + str(number_paths))
+        col1.plotly_chart(fig,use_container_width=True)
+
+        col2.text("Average PnL, hedged LP with knockin put: ") 
+        col3.text(mc_paths["PnL Knockin Put Hedge"].mean())
+        col2.text("Median PnL, hedged LP with knockin put: ") 
+        col3.text(mc_paths["PnL Knockin Put Hedge"].median())
+        col2.text("5th percentile PnL, hedged LP with knockin put: ") 
+        col3.text(mc_paths["PnL Knockin Put Hedge"].quantile(.05))
+        col2.text("1st percentile PnL, hedged LP with knockin put: ") 
+        col3.text(mc_paths["PnL Knockin Put Hedge"].quantile(.01))                
+
+        mc_paths["Price End"] = mc_paths[M]
+        values_put_vanilla = []
+        values_put_knockin = []
+
+        for i in range(0, len(mc_paths)):
+            price = float(mc_paths["Price End"][i])
+            values_put_vanilla.append(max(0,P_put_new-price))
+            values_put_knockin.append(P_put_new-price if price <= B_knockin else 0)
+            
+        mc_paths["Payoff Vanilla Put"] = values_put_vanilla
+        mc_paths["Payoff Knockin Put"] = values_put_knockin
+
+        def euro_option(S, K, T, r, sigma, option = 'call'):
+            
+            #S: spot price
+            #K: strike price
+            #T: time to maturity
+            #r: interest rate
+            #sigma: volatility of underlying asset
+            
+            d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+            d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+            
+            if option == 'call':
+                result = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+            if option == 'put':
+                result = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
+            if option == 'binary put':
+                result = (np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0))        
+                
+            return result
+
+        df_cost_of_hedge = pd.DataFrame([["Vanilla put", euro_option(P, P_put_new, T, r, sigma, 'put'), x_min_new, x_min_new*euro_option(P, P_put_new, T, r, sigma, 'put')], 
+                                        ["Knockin put", (P_put_new - B_knockin) * euro_option(P, B_knockin, T, r, sigma, 'binary put') + euro_option(P, B_knockin, T, r, sigma, 'put'), x_min_new, x_min_new*((P_put_new - B_knockin) * euro_option(P, B_knockin, T, r, sigma, 'binary put') + euro_option(P, B_knockin, T, r, sigma, 'put'))]],
+                                        columns = ["Hedging instrument", "Theoretical price", "Number of instruments", "Theoretical hedging cost"])
+        st.dataframe(df_cost_of_hedge)
+
+        df_put_prices = pd.DataFrame([["Vanilla put", euro_option(P, P_put_new, T, r, sigma, 'put'), np.exp(-r*T)*np.mean(values_put_vanilla)], 
+                                        ["Knockin put", (P_put_new - B_knockin) * euro_option(P, B_knockin, T, r, sigma, 'binary put') + euro_option(P, B_knockin, T, r, sigma, 'put'),np.exp(-r*T)*np.mean(values_put_knockin)]],
+                                        columns = ["Hedging instrument", "Analytical (Black-Scholes) price", "Monte Carlo estimate"])
+        st.dataframe(df_put_prices)
---
+++ knockin_put_pricer.py
@@ -0,0 +1,96 @@
+import streamlit as st
+st.set_page_config(page_title="Black Scholes Pricer, Knockin Put Option", layout="wide")
+import numpy as np
+import pandas as pd
+import scipy.stats as si
+import plotly.express as px
+import plotly.graph_objects as go
+
+st.title("Black Scholes Pricer, Knockin Put Option")
+
+P = st.number_input('Current Price', value=1666.000, step = 0.001)
+K_put = st.number_input('Put Strike', value=1470.000, step = 0.001)
+B_knockin = st.number_input('Down and In Barrier', value=1333.000, step = 0.001)
+numberdays = st.number_input('Number of days', value=7.00, step = .01)
+T = numberdays/365.25
+
+#enter drift (risk-free rate for option pricing)
+r_in_percent = st.number_input('Drift in % (risk-free rate for option pricing)', value=0.0000, step = 0.0001)
+r = r_in_percent/100
+sigma_B_in_percent = st.number_input('Implied Volatility at down and in barrier in %', value = 90.000, step=0.0001)
+sigma_B = sigma_B_in_percent / 100
+sigma_K_in_percent = st.number_input('Implied Volatility at put strike, in %', value = 90.000, step=0.0001)
+sigma_K = sigma_B_in_percent / 100
+
+def euro_option(S, K, T, r, sigma, option = 'call'):
+    
+    #S: spot price
+    #K: strike price
+    #T: time to maturity
+    #r: interest rate
+    #sigma: volatility of underlying asset
+    
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    
+    if option == 'call':
+        result = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+    if option == 'put':
+        result = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) - S * si.norm.cdf(-d1, 0.0, 1.0))
+    if option == 'binary put':
+        result = (np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0))        
+        
+    return result
+
+def knockin_put(S, K, T, r, sigma, B):
+  return (K - B) * euro_option(S, B, T, r, sigma, 'binary put') + euro_option(P, B, T, r, sigma, 'put')
+
+lower_bound = 0
+upper_bound = 2*K_put
+step = upper_bound/10000
+prices = np.arange(lower_bound,upper_bound+1,step)
+
+put_vanilla_barrier_strike = []
+put_binary = []
+put_knockin = []
+put_vanilla = []
+for price in prices:
+    put_vanilla_barrier_strike.append(max(0,B_knockin-price))
+    put_binary.append(K_put - B_knockin if price <= B_knockin else 0)
+    put_knockin.append(K_put-price if price <= B_knockin else 0)
+    put_vanilla.append(max(0,K_put-price))    
+
+df = pd.DataFrame()
+df["Price"] = prices
+df["Vanilla Put Payoff, Barrier Strike"] = put_vanilla_barrier_strike
+df["Binary Put Payoff"] = put_binary
+df["Knockin Put Payoff"] = put_knockin
+df["Vanilla Put Payoff"] = put_vanilla
+
+col1, col2 = st.columns(2)
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=df["Price"], y=df["Vanilla Put Payoff, Barrier Strike"], mode='lines', name="Vanilla Put Payoff"))
+fig.add_trace(go.Scatter(x=df["Price"], y=df["Binary Put Payoff"], mode='lines', name="Binary Put Payoff"))
+fig.add_trace(go.Scatter(x=df["Price"], y=df["Knockin Put Payoff"], mode='lines', name="Knockin Put Payoff"))
+
+
+fig.update_layout(title=dict(text='Options payoff: Decomposition of Knockin put option = Vanilla put option + binary put option<br>Knock in put: Strike = ' +str(K_put) + ", Barrier = " +str(B_knockin) + '<br>Vanilla put: Strike = ' +str(B_knockin)+'<br>Binary put: Strike = ' +str(B_knockin) + ', Notional = ' +str(K_put - B_knockin),
+font=dict(size=12)))
+fig.update_xaxes(title_text='Price')
+fig.update_yaxes(title_text='Payoff')
+col1.plotly_chart(fig,use_container_width=True)
+
+fig = px.line(df, x="Price", y="Vanilla Put Payoff", title='Vanilla Put option, Strike = ' +str(K_put))
+fig.update_xaxes(title_text='Price')
+fig.update_yaxes(title_text='Payoff')
+col2.plotly_chart(fig,use_container_width=True)
+
+df = pd.DataFrame([["Vanilla put, Strike = " + str(B_knockin), euro_option(P, B_knockin, T, r, sigma_B, 'put')],
+                   ["Binary put, Strike = "+ str(B_knockin) +", Notional = " +str(K_put - B_knockin), (K_put - B_knockin) * euro_option(P, B_knockin, T, r, sigma_B, 'binary put')],
+                  ["Knockin put, Strike = "+ str(K_put) +", lower barrier = " +str(B_knockin) + " (Sum of above two options)", knockin_put(P, K_put, T, r, sigma_B, B_knockin)],
+                  ["Compare with: Vanilla put, Strike = " + str(K_put), euro_option(P, K_put, T, r, sigma_B, 'put')]],
+                  columns=["Hedging Instrument", "Black-Scholes Price"])
+
+st.dataframe(df)
+
+st.write("Discount: " ,   "{:.02%}".format(1-knockin_put(P, K_put, T, r, sigma_B, B_knockin)/euro_option(P, K_put, T, r, sigma_B, 'put')) )
